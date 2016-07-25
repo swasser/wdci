d8eda4f5ec5bfa7586937223ec49c345e6ae4b44
eviltester <3K1aDZQQgLtvBXuI>
2016-07-25 12:47:25 +0100

    added subset of course tests to a CI repo for public access without having to create an svn account

---
+++ code/src/test/java/com/seleniumsimplified/webdriver/basics/scratchpad/Scratchpad.java
@@ -0,0 +1,22 @@
+package com.seleniumsimplified.webdriver.basics.scratchpad;
+
+import org.junit.Ignore;
+import org.openqa.selenium.WebDriver;
+
+
+/**
+ * This is not a test in the main course.
+ *
+ * It is a file that I add for experimenting with code.
+ *
+ * It should be ignored.
+ *
+ * The code in here may not actually work.
+ */
+@Ignore("Because this is a scratchpad and I only want to use it when I need it, it might have random stuff in it")
+public class Scratchpad {
+
+    private static WebDriver driver;
+
+
+}
